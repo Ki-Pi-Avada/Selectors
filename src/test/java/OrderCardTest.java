794c2ddd10f32dc022d01f2d087083a827691ccd
Kifo <i.g.a.n.garsia@gmail.com>
2025-05-09 18:40:46 +0300

    Add test

---
+++ src/test/java/OrderCardTest.java
@@ -0,0 +1,54 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import net.bytebuddy.dynamic.scaffold.TypeWriter;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class OrderCardTest {
+
+    WebDriver driver;
+
+    @BeforeAll
+    public static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999/");
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldSendForm() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иван Иванов");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79111118483");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("button[type=button]")).click();
+        WebElement successText = driver.findElement(By.cssSelector("[data-test-id=order-success]"));
+        assertTrue(successText.isDisplayed());
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", successText.getText().trim());
+    }
+}
+
